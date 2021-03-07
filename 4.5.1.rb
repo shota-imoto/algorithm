c8674f5342838d1b84c1ea96da5cd6ce71078d64
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2021-03-07 17:21:29 +0900

    部分和問題のリファクタリング

--- 4.5.1.rb
+++ 4.5.1.rb
@@ -8,22 +8,17 @@ class PartOfSum
     @cache = {}
   end
 
+  # キャッシュなし
   def check_sumable?(w)
     _check_sumable?(array.size, w)
   end
 
   def _check_sumable?(i, w)
-    if i == 0
-      if w == 0
-        return true
-      else
-        return false
-      end
-    end
-
-    false
+    return w == 0 if i == 0
+    return _check_sumable?(i-1, w) || _check_sumable?(i-1, w-array[i-1])
   end
 
+  # キャッシュあり
   def check_sumable_cache?(w)
     _check_sumable_cache?(array.size, w)
   end
@@ -34,8 +29,7 @@ class PartOfSum
     cache["#{i-1}-#{w}"] = _check_sumable_cache?(i-1, w) if cache["#{i-1}-#{w}"].nil?
     cache["#{i-1}-#{w-array[i-1]}"] = _check_sumable_cache?(i-1, w-array[i-1]) if cache["#{i-1}-#{w-array[i-1]}"].nil?
 
-    return true if cache["#{i-1}-#{w}"] || cache["#{i-1}-#{w-array[i-1]}"]
-    false
+    return cache["#{i-1}-#{w}"] || cache["#{i-1}-#{w-array[i-1]}"]
   end
 end
 
@@ -43,7 +37,7 @@ array = Generator.random_array(1..20, 8)
 part_of_sum = PartOfSum.new(array)
 pp "配列：#{array}"
 n = gets.to_i
-pp part_of_sum.check_sumable_cache?(n)
+pp part_of_sum.check_sumable?(n)
 
 # メモ
 # 現状の実装だとw-array[i-1]が負の値となるの分岐に入ったあとも、処理を継続してしまう
